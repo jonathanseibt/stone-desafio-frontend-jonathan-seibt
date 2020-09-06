26091558f996b172ef0030e0d23a855a8f6e9529
Jonathan Seibt <jonathanvseibt@gmail.com>
2020-09-06 19:21:12 -0300

    chore: Install create-react-app

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    Hello Stone
+  </React.StrictMode>,
+
+  document.getElementById('root')
+);
