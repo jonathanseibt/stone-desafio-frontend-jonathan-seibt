f04d6526cafb3aa2f2dc310b4af6cc08128a399e
Jonathan Seibt <jonathanvseibt@gmail.com>
2020-09-06 19:39:32 -0300

    style: Format code w/ Prettier

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    Hello Stone
-  </React.StrictMode>,
+  <React.StrictMode>Hello Stone</React.StrictMode>,
 
-  document.getElementById('root')
+  document.getElementById('root'),
 );
