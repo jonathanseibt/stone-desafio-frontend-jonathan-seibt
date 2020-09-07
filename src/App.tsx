28b8a04266dce81682130a6906f83fba92a7da55
Jonathan Seibt <jonathanvseibt@gmail.com>
2020-09-06 23:20:20 -0300

    feat: Setup Material-UI theme on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { observer } from 'mobx-react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import Theme from './Theme';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 const App: React.FC = observer(() => {
-  return <>Hello Stone</>;
+  return (
+    <ThemeProvider theme={Theme}>
+      <CssBaseline />
+      <>Hello Stone</>
+    </ThemeProvider>
+  );
 });
 
 export default App;
