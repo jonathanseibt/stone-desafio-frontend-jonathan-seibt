0b6e48e08f1bca13205f48e27d2b3d294c941939
Jonathan Seibt <jonathanvseibt@gmail.com>
2020-09-13 16:37:59 -0300

    feat: Add Material-UI theme customization

--- src/Theme.ts
+++ src/Theme.ts
@@ -1,5 +1,31 @@
 import { createMuiTheme } from '@material-ui/core/styles';
+import { AvatarGroupClassKey } from '@material-ui/lab/AvatarGroup';
 
-const Theme = createMuiTheme({});
+declare module '@material-ui/core/styles/overrides' {
+  export interface ComponentNameToClassKey {
+    MuiAvatarGroup: AvatarGroupClassKey;
+  }
+}
+
+const Theme = createMuiTheme({
+  overrides: {
+    MuiTabs: {
+      indicator: {
+        backgroundColor: 'white',
+      },
+    },
+    MuiAvatarGroup: {
+      avatar: {
+        border: 0,
+      },
+    },
+    MuiAvatar: {
+      root: {
+        width: '44px',
+        height: '44px',
+      },
+    },
+  },
+});
 
 export default Theme;
