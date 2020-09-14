b8e5a8cfe719c79d6b95c105804d4a3c076d354e
Jonathan Seibt <jonathanvseibt@gmail.com>
2020-09-14 05:29:57 -0300

    feat: Add Cryptos list to LocalStorage

--- src/Browser.store.ts
+++ src/Browser.store.ts
@@ -1,11 +1,13 @@
 import { observable } from 'mobx';
 import { persist } from 'mobx-persist';
+import { CryptoModel } from './Models/Crypto/Crypto.model';
 import { UserModel } from './Models/User/User.model';
 
 class BrowserStore {
   @observable hydrated = false;
 
   @persist('list') @observable users: UserModel[] = [];
+  @persist('list') @observable cryptos: CryptoModel[] = [];
 }
 
 export default new BrowserStore();
