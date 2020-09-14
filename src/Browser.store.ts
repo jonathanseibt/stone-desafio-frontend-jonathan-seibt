74eb430c33bbc6c21d082c12c9f9ed23115c21ad
Jonathan Seibt <jonathanvseibt@gmail.com>
2020-09-14 05:29:42 -0300

    feat: Add Users list to local storage

--- src/Browser.store.ts
+++ src/Browser.store.ts
@@ -1,7 +1,11 @@
 import { observable } from 'mobx';
+import { persist } from 'mobx-persist';
+import { UserModel } from './Models/User/User.model';
 
 class BrowserStore {
   @observable hydrated = false;
+
+  @persist('list') @observable users: UserModel[] = [];
 }
 
 export default new BrowserStore();
