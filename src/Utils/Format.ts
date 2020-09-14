149e0da24b9266d5a2ab6e71202b1a0e3d703bd7
Jonathan Seibt <jonathanvseibt@gmail.com>
2020-09-14 17:13:26 -0300

    feat: Add utility class for formating

---
+++ src/Utils/Format.ts
@@ -0,0 +1,27 @@
+class Format {
+  static LOCALE = 'pt-BR';
+  static CURRENCY = 'BRL';
+
+  static real = (value: number | undefined): string => {
+    value = value ? Number(value) : 0;
+
+    return value.toLocaleString(Format.LOCALE, { style: 'currency', currency: Format.CURRENCY, minimumFractionDigits: 2 });
+  };
+
+  static decimal = (value: number | undefined, digits?: number): string => {
+    value = value ? Number(value) : 0;
+    digits = digits ? Number(digits) : 2;
+
+    return value.toLocaleString(Format.LOCALE, { style: 'decimal', minimumFractionDigits: digits });
+  };
+
+  static date = (value: Date): string => {
+    return value ? value.toLocaleDateString(Format.LOCALE) : '';
+  };
+
+  static time = (value: Date): string => {
+    return value ? value.toLocaleTimeString(Format.LOCALE) : '';
+  };
+}
+
+export default Format;
