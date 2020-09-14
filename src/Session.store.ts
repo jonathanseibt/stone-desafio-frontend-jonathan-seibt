bc28e80f2cef9c073204ce325a5a3e28ab069059
Jonathan Seibt <jonathanvseibt@gmail.com>
2020-09-14 05:30:25 -0300

    feat: Setup Session store

--- src/Session.store.ts
+++ src/Session.store.ts
@@ -1,3 +1,21 @@
-class SessionStore {}
+import { observable } from 'mobx';
+import { persist } from 'mobx-persist';
+import { UserModel } from './Models/User/User.model';
+
+class SessionStore {
+  @persist @observable _user: string = '';
+
+  isAuthenticated = (): boolean => {
+    return !!this._user;
+  };
+
+  unauthenticate = () => {
+    this._user = '';
+  };
+
+  getUser = (): UserModel => {
+    return UserModel.findByID(this._user) ?? new UserModel();
+  };
+}
 
 export default new SessionStore();
