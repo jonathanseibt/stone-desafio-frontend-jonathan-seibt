f74353526ac13cb644ac66c2cbf3ddf5617537f2
Jonathan Seibt <jonathanvseibt@gmail.com>
2020-09-14 05:25:32 -0300

    feat: Add constants for cryptos details

--- src/Constants.ts
+++ src/Constants.ts
@@ -2,6 +2,20 @@ class Constants {
   static TITLE = 'E-Wallet';
 
   static NEW_USERS_GIFT_BALANCE = 100000;
+
+  static BITCOIN = {
+    name: 'Bitcoin',
+    acronym: 'BTC',
+    icon: 'bitcoin.png',
+    backgroundStyle: 'linear-gradient(90deg, rgba(255,232,222,0.5) 0%, rgba(255,255,255,0.5) 50%)',
+  };
+
+  static BRITA = {
+    name: 'Brita',
+    acronym: 'BRT',
+    icon: 'brita.png',
+    backgroundStyle: 'linear-gradient(90deg, rgba(223,247,242,0.5) 0%, rgba(255,255,255,0.5) 50%)',
+  };
 }
 
 export default Constants;
