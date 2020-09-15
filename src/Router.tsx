bc9f41ed7a8b02a95ce0aa678be64bb9abd64439
Jonathan Seibt <jonathanvseibt@gmail.com>
2020-09-15 04:46:50 -0300

    chore: Test GitHub Pages SPA deploy on BrowserRouter

--- src/Router.tsx
+++ src/Router.tsx
@@ -14,7 +14,7 @@ import WalletView, { TITLE as WALLET_VIEW_TITLE, URL as WALLET_VIEW_URL } from '
 
 const Router: React.FC = observer(() => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       {BrowserStore.hydrated && (
         <Switch>
           <Route exact path='/'>
